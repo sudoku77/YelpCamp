1588bddf64fa99fca36cf027143eda42f506b4df
sudoku77 <echoqishu@gmail.com>
2020-06-27 06:23:12 +0000

    add campground to seeds

--- seeds.js
+++ seeds.js
@@ -32,6 +32,16 @@ var data = [
             username: "Fye"
         },
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+	    {
+        name: "Above Cloud", 
+		price: "200",
+        image: "https://i.imgur.com/8EHGA.jpeg",
+		author:{
+            id : "588c2e092403d111454fff25",
+            username: "Fye"
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     }
 ]
 
