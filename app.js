26bacf51dfc67949cae174de66af92f2fb1c41c8
sudoku77 <echoqishu@gmail.com>
2020-06-27 06:05:25 +0000

    add mongodb atlas

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser:true,useUnifiedTopology: true});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser:true,useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://devsproud:password123456789@cluster0.8pwkm.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
